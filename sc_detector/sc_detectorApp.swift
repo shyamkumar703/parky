5092bcdd6a24f5b6c46c598023b8704d5558069e
Shyam Kumar <shyamkumar@berkeley.edu>
2026-02-08 10:03:07 -0800

    Initial Commit

---
+++ sc_detector/sc_detectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  sc_detectorApp.swift
+//  sc_detector
+//
+//  Created by Shyam Kumar on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct sc_detectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sc_detectorUITests/sc_detectorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  sc_detectorUITestsLaunchTests.swift
+//  sc_detectorUITests
+//
+//  Created by Shyam Kumar on 2/8/26.
+//
+
+import XCTest
+
+final class sc_detectorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
